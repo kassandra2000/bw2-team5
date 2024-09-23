d3a6fb612e4e2c16a6d6451fe73a9fecea82ab52
angelolaera <angelo.laera.1995@gmail.com>
2024-09-23 12:26:17 +0200

    Classi + CSV dependency

--- src/main/java/kassandrafalsitta/bw2/entities/Cliente.java
+++ src/main/java/kassandrafalsitta/bw2/entities/Cliente.java
@@ -12,15 +12,10 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 @Table(name = "clienti")
 
-public class Cliente {
-    @Id
-    @Setter(AccessLevel.NONE)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    UUID id;
+public class Cliente extends Utente {
 
     private String ragioneSociale;
     private String partitaIva;
@@ -41,4 +36,23 @@ public class Cliente {
 
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Fattura> fatture;
+
+    public Cliente(UUID id, String username, String email, String password, String nome, String cognome, String avatar, String ragioneSociale, String partitaIva, LocalDate dataInserimento, LocalDate dataUltimoContatto, BigInteger fatturatoAnnuale, String pec, String telefono, String emailContatto, String nomeContatto, String cognomeContatto, String telefonoContatto, String logoAziendale, String tipoClienti, List<Indirizzo> indirizzi, List<Fattura> fatture) {
+        super(id, username, email, password, nome, cognome, avatar);
+        this.ragioneSociale = ragioneSociale;
+        this.partitaIva = partitaIva;
+        this.dataInserimento = dataInserimento;
+        this.dataUltimoContatto = dataUltimoContatto;
+        this.fatturatoAnnuale = fatturatoAnnuale;
+        this.pec = pec;
+        this.telefono = telefono;
+        this.emailContatto = emailContatto;
+        this.nomeContatto = nomeContatto;
+        this.cognomeContatto = cognomeContatto;
+        this.telefonoContatto = telefonoContatto;
+        this.logoAziendale = logoAziendale;
+        this.tipoClienti = tipoClienti;
+        this.indirizzi = indirizzi;
+        this.fatture = fatture;
+    }
 }
